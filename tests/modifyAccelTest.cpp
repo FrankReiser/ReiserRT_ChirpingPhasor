b9d3dc6e027fa2dbbbc7e057dc05b668f7b686e3
Frank Reiser <Frank@TheReisers.com>
2022-11-07 15:25:57 -0500

    Initial Modify Acceleration Test
    I may want to analyze this over shorter epoch periods as imperfections add up. A more granular testing may be necessary.

--- tests/modifyAccelTest.cpp
+++ tests/modifyAccelTest.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "ChirpingPhasorToneGenerator.h"
+#include "MiscTestUtilities.h"
 
 #include <memory>
 #include <iostream>
@@ -10,9 +11,9 @@
 int main()
 {
     // An arbitrary epoch dwell in samples.
-    constexpr size_t NUM_SAMPLES = 4;
-    constexpr auto initialAccel = M_PI / NUM_SAMPLES;
-    constexpr auto secondAccel = M_PI / NUM_SAMPLES / 2;
+    constexpr size_t NUM_SAMPLES = 8192;
+    constexpr auto initialAccel = M_PI / NUM_SAMPLES / 2;
+    constexpr auto secondAccel = -initialAccel / 2;
 
     int retCode = 0;
 
@@ -22,21 +23,59 @@ int main()
     using SampleType = ReiserRT::Signal::FlyingPhasorElementType;
     std::unique_ptr< SampleType[] > chirpBuf{ new SampleType[ NUM_SAMPLES ] };
 
-    // Get data from each of the tone generator.
-    chirpGen.getSamples( chirpBuf.get(), NUM_SAMPLES );
-
     // Do once construct
     do {
-        // Omega N should equal initialAccel * N to a high degree.
-        auto expectedOmegaN = initialAccel * (NUM_SAMPLES-1);
+        std::cout.precision(6);
 
-        // Get the phase of the last sample and query the angular velocity
-        auto endPhase = std::arg( chirpBuf[NUM_SAMPLES-1] );
-//        auto omegaN = chirpGen.getOmegaN();
+        // Get data from each of the tone generator.
+        chirpGen.getSamples( chirpBuf.get(), NUM_SAMPLES );
 
-//        if ( expectedOmegaN != omegaN )
-//            std::cout << "BLAH" << std::endl;
+        // Get the Angular Velocity of the Next Sample to be retrieved.
+        auto omegaN1 = chirpGen.getOmegaN();
+        std::cout << "First Epoch Retrieved. OmegaN = " << omegaN1 << std::endl;
+        if ( !inTolerance( omegaN1, initialAccel * NUM_SAMPLES, 1e-10 ) )
+        {
+            std::cout << "Chirping Phasor FAILS First Epoch Retrieval Omega Test, Expected: " << initialAccel * NUM_SAMPLES
+                      << ", Detected: " << omegaN1 << std::endl;
+            retCode = 1;
+            break;
+        }
 
+        // Modify Acceleration to 0 acceleration (coast)
+        chirpGen.modifyAccel();
+
+        // Get data from each of the tone generator.
+        chirpGen.getSamples( chirpBuf.get(), NUM_SAMPLES );
+
+        // Get the Angular Velocity of the Next Sample to be retrieved.
+        // It should be roughly the same as the original angular velocity.
+        auto omegaN2 = chirpGen.getOmegaN();
+        std::cout << "Second Epoch Retrieved. OmegaN = " << omegaN2 << std::endl;
+        if ( !inTolerance( omegaN2, omegaN1, 1e-4 ) )
+        {
+            std::cout << "Chirping Phasor FAILS Second Epoch Retrieval Omega Test, Expected: " << omegaN1
+                      << ", Detected: " << omegaN2 << std::endl;
+            retCode = 2;
+            break;
+        }
+
+        // Modify Acceleration to inverse half of original (decelerate more slowly).
+        chirpGen.modifyAccel( secondAccel );
+
+        // Get data from each of the tone generator.
+        chirpGen.getSamples( chirpBuf.get(), NUM_SAMPLES );
+
+        // Get the Angular Velocity of the Next Sample to be retrieved.
+        // It should be roughly half as the original velocity.
+        auto omegaN3 = chirpGen.getOmegaN();
+        std::cout << "Third Epoch Retrieved. OmegaN = " << omegaN3 << std::endl;
+        if ( !inTolerance( omegaN3, omegaN1 / 2.0, 1e-3 ) )
+        {
+            std::cout << "Chirping Phasor FAILS Third Epoch Retrieval Omega Test, Expected: " << omegaN1 / 2.0
+                      << ", Detected: " << omegaN3 << std::endl;
+            retCode = 2;
+            break;
+        }
 
     } while (false);
 
