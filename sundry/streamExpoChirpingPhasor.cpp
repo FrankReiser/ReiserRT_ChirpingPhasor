1d65289f1f153158b49273c6009e0917a8c73525
Frank Reiser <Frank@TheReisers.com>
2023-11-29 15:46:31 -0500

    Skeleton for Streaming Expo Chirping Phasor
    We do not have an Expo Chirping Phasor yet but, I need to start slinging some experimental code for this thing and I need a program to do that.

---
+++ sundry/streamExpoChirpingPhasor.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file streamExpoChirpingPhasor.cpp
+ * @brief Utility program for generating output from a ExpoChirpingPhasor for whatever analytical purposes.
+ *
+ * @authors Frank Reiser
+ * @date Initiated on Nov 29, 2023
+ */
+
+#include <iostream>
+
+int main( int argc, char * argv[] )
+{
+
+
+
+    return 0;
+    exit( 0 );
+}
